32188d492c67dcbd70f92d1a7b0ab6922a106acc
Igor Leshenko <jungoigorl@gmail.com>
2013-07-19 12:37:36 +0300

    Better device discovery

--- pull_log.py
+++ pull_log.py
@@ -6,29 +6,34 @@ SERIAL_MAX= 50
 SERIAL_TIMEOUT = 0.1
 SERIAL_BAUDRATE = 115200
 
+class NoDeviceError(Exception):
+    pass
+
 def find_device():
-    """Iterates over com ports and ping everyone until get's ping
+    """Iterates over com ports and ping everyone until get's reply
     from the real device. Returns a serial socket."""
     for i in xrange(SERIAL_MAX):
-        device_found = False
-        s = None
         try:
-            s = Serial(i, timeout=SERIAL_TIMEOUT, baudrate=SERIAL_BAUDRATE)
-            s.write('p')
-            if s.readline().strip() == 'P':
-                device_found = True
-                return s
+            with Serial(i, timeout=SERIAL_TIMEOUT, baudrate=SERIAL_BAUDRATE) as s:
+                s.write('p')
+                if s.readline().strip() == 'P':
+                    break
         except SerialException:
             pass
-        finally:
-            # If the device was found we can't destroy it because it's
-            # returned from the function
-            if not device_found and s:
-                s.close()
+    else:
+        raise NoDeviceError()
+
+    return Serial(i, timeout=SERIAL_TIMEOUT, baudrate=SERIAL_BAUDRATE)
+
+def pull_log():
+    with find_device() as device:
+        print device
 
 def main():
-    device = find_device()
-    print device
+    try:
+        log = pull_log()
+    except NoDeviceError:
+        print 'Device not found'
 
 if __name__ == '__main__':
     main()
